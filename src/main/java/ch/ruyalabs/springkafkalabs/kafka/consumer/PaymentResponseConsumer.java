a3fd349b7470c1ca22bb4a4dba4953a7da15a5d2
ruyalabs <dev@ruyalabs.ch>
2025-07-01 00:02:23 +0200

    Introduce Payment Kafka producers/consumers and add CloudEvents dependencies
    - Added `PaymentRequestProducer` and `PaymentResponseConsumer` components.
    - Integrated CloudEvents dependencies (core, JSON, and Kafka).
    - Renamed JSON schema files for improved clarity.

---
+++ src/main/java/ch/ruyalabs/springkafkalabs/kafka/consumer/PaymentResponseConsumer.java
@@ -0,0 +1,8 @@
+package ch.ruyalabs.springkafkalabs.kafka.consumer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentResponseConsumer {
+
+}
---
+++ src/main/java/ch/ruyalabs/springkafkalabs/kafka/producer/PaymentRequestProducer.java
@@ -0,0 +1,9 @@
+package ch.ruyalabs.springkafkalabs.kafka.producer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentRequestProducer {
+
+
+}
